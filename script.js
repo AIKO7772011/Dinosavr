4abebf53fd7484643d9ad2212558ff63ce615128
AIKO7772011 <isystem107@mail.ru>
2021-02-17 00:18:45 +0500

    ch 160221 T0018

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ let isAlive = setInterval (function() {
     let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
     console.log(dinoTop);
 
-    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+    if (cactusLeft < 40 && cactusLeft > 0 && dinoTop >= 140) {
         alert("простите но, вы проиграли!!!");
         console.log(cactusLeft+"cactus");
     }
