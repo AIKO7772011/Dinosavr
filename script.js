e7b5c664db122493235882a96528981c08dc2d94
AIKO7772011 <64735317+AIKO7772011@users.noreply.github.com>
2021-03-17 04:18:01 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus");
 
+var tID; //we will use this variable to clear the setInterval()
 
+function stopAnimate() {
+	clearInterval(tID);
+} //end of stopAnimate()
+
+function animateScript() {
+	var    position = 88; //start position for the image slicer
+	const  interval = 100; //100 ms of interval for the setInterval()
+	const  diff = 88;     //diff as a variable for position offset
+	tID = setInterval ( () => { document.getElementById("dino").style.backgroundPosition = `${position}px 0px`; 
+	//we use the ES6 template literal to insert the variable "position"
+	if (position < 528)
+	{ position = position + diff;}
+	//we increment the position by 256 each time
+	else
+	{ position = 88; }
+	//reset the position to 256px, once position exceeds 1536px
+	}
+	, interval ); //end of setInterval
+} //end of animateScript()
 
 document.addEventListener("click", function(event){
     jump();
